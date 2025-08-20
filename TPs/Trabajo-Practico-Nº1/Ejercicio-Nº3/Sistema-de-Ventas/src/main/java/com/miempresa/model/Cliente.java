f916ca014e06f69cac8300f9fd5e8947314d3be1
FacundoL <lella.facundo.4e@gmail.com>
2025-08-20 20:53:05 -0300

    Update Ejercicio3

--- TPs/Trabajo-Practico-Nº1/Ejercicio-Nº3/Sistema-de-Ventas/src/main/java/com/miempresa/model/Cliente.java
+++ TPs/Trabajo-Practico-Nº1/Ejercicio-Nº3/Sistema-de-Ventas/src/main/java/com/miempresa/model/Cliente.java
@@ -13,6 +13,8 @@ import java.io.Serializable;
 @Table (name = "Cliente")
 public class Cliente implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
