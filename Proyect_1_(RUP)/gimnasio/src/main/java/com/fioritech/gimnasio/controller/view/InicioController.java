62a29aca7fe2396dbd4fc4b56525497ae39e53e9
FacundoL <lella.facundo.4e@gmail.com>
2025-09-20 11:53:06 -0300

    interfaz inicial

---
+++ Proyect_1_(RUP)/gimnasio/src/main/java/com/fioritech/gimnasio/controller/view/InicioController.java
@@ -0,0 +1,12 @@
+package com.fioritech.gimnasio.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InicioController {
+    @GetMapping("/")
+	public String inicio() {
+		return "view/inicio";
+	}
+}
