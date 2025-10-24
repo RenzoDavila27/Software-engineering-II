8a6f571e24d39cc63d57b2ec8503ea313dd6c462
Alejandro <alemoreno2538@gmail.com>
2025-10-24 18:48:09 -0300

    comienzo

---
+++ Practice/tp2/Ex5/ExB/biblioteca/biblioteca/src/main/java/com/is/biblioteca/SeguridadWeb.java
@@ -0,0 +1,53 @@
+package com.is.biblioteca;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.is.biblioteca.business.logic.service.UsuarioService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SeguridadWeb {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usuarioService)
+            .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/admin/*").hasRole("ADMIN")
+                .requestMatchers("/css/**", "/js/**", "/img/**", "/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .loginProcessingUrl("/logincheck")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/inicio")
+                .permitAll()
+                )
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll()
+            )
+            .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
