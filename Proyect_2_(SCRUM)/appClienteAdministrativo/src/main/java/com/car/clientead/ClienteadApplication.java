4c69ef167ca6af27b47e50c2cecaac5133adad66
FacundoLella <lella.facundo.4e@gmail.com>
2025-11-07 19:00:19 -0300

    Start Proyect 2

---
+++ Proyect_2_(SCRUM)/appClienteAdministrativo/src/main/java/com/car/clientead/ClienteadApplication.java
@@ -0,0 +1,13 @@
+package com.car.clientead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteadApplication.class, args);
+	}
+
+}
---
+++ Proyect_2_(SCRUM)/appClientePromocional/src/main/java/com/car/clientep/ClientepApplication.java
@@ -0,0 +1,13 @@
+package com.car.clientep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientepApplication.class, args);
+	}
+
+}
